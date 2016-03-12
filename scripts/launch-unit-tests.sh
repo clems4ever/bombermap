67ba30971cdb427ca017b87d44f7a96a0c76229b
Clément Michaud <clement.michaud34@gmail.com>
2016-03-12 21:22:32 +0100

    Improve launch-unit-tests.sh script

--- scripts/launch-unit-tests.sh
+++ scripts/launch-unit-tests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ "$WARGAME_CLIENT_DIRECTORY" == "" ]
+then
+	echo "WARGAME_CLIENT_DIRECTORY not set"
+        exit 1
+fi
+
 docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $WARGAME_CLIENT_DIRECTORY:/data -e TERM=dumb registry.ingenious-cm.fr/wargame_devenv /bin/bash -c "cd /data && ./gradlew test"
