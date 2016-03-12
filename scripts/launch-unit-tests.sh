fc4f1a7f97b00d24f0a7a76d437f284165fd317b
Clément Michaud <clement.michaud34@gmail.com>
2016-03-12 22:11:12 +0100

    Publish unit tests results

--- scripts/launch-unit-tests.sh
+++ scripts/launch-unit-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+RESULTS_DIRECTORY=`pwd`
+
 if [ "$WARGAME_CLIENT_DIRECTORY" == "" ]
 then
 	echo "WARGAME_CLIENT_DIRECTORY not set"
         exit 1
 fi
 
-docker run --rm -e ANDROID_HOME=/home/developer/Android/Sdk -v $WARGAME_CLIENT_DIRECTORY:/data -e TERM=dumb registry.ingenious-cm.fr/wargame_devenv /bin/bash -c "cd /data && ./gradlew test"
+docker run --rm -e ANDROID_HOME=/home/developer/Android/Sdk -v $WARGAME_CLIENT_DIRECTORY:/data -v $RESULTS_DIRECTORY:/test-results -e TERM=dumb registry.ingenious-cm.fr/wargame_devenv /bin/bash -c "cd /data && ./gradlew test || cp -r /data/app/build/test-results/debug/* /test-results"
