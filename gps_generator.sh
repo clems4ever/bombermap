9686ff8d102fd367841c76aedb84bebba06e06ec
sergei <serge.kireev.fr@gmail.com>
2016-03-17 21:21:22 +0100

    gps generation in paris by default, and possibility to offset the player from default position

--- gps_generator.sh
+++ gps_generator.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 port=$1
+latoffset=$2
+longoffset=$3
 
 parislat=2.3228797
 parislong=48.8870407
 # 2.3228797 48.8870407
+g++ generatecoords.cpp -o geogen
+chmod +x geogen
+
+if [ -z "$2" ]
+  then
+    echo "now you can set the offset of a player from basic position :"
+    echo "try ./gps_generator.sh 5554 0 0 and ./gps_generator.sh 5556 20 20   "
+
+else
 
 while [ 1 ]
 do
@@ -15,10 +26,9 @@ do
 #cecho $longitude
 
 # 5554
-g++ generatecoords.cpp -o geogen
-chmod +x geogen
 
-./geogen
-./geogen | telnet localhost $port 
+./geogen $latoffset $longoffset
+./geogen $latoffset $longoffset | telnet localhost $port 
 sleep 1
 done
+fi
