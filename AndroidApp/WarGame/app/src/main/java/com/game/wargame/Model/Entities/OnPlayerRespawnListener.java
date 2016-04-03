4c6e6dfcf8895926676e3b24faf1b74f4dc5f2e3
sergei <serge.kireev.fr@gmail.com>
2016-04-03 13:02:51 +0200

    added OnPlayerRespawnListener

---
+++ AndroidApp/WarGame/app/src/main/java/com/game/wargame/Model/Entities/OnPlayerRespawnListener.java
@@ -0,0 +1,8 @@
+package com.game.wargame.Model.Entities;
+
+/**
+ * Created by sergei on 15/03/16.
+ */
+public interface OnPlayerRespawnListener {
+    public void onRespawn(String playerId, double time);
+}
