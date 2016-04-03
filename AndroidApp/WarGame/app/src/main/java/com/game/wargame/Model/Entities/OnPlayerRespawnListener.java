f3fba9710c83ec6c2e8dbb62f6a89312bd592239
sergei <serge.kireev.fr@gmail.com>
2016-04-03 13:08:48 +0200

    move playerListeners

--- AndroidApp/WarGame/app/src/main/java/com/game/wargame/Model/Entities/OnPlayerRespawnListener.java
+++
@@ -1,8 +0,0 @@
-package com.game.wargame.Model.Entities;
-
-/**
- * Created by sergei on 15/03/16.
- */
-public interface OnPlayerRespawnListener {
-    public void onRespawn(String playerId, double time);
-}
