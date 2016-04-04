895073444db2a8304d7546e322186510df61bd16
sergei <serge.kireev.fr@gmail.com>
2016-04-05 00:00:15 +0200

    adapted tests for new signature

--- AndroidApp/WarGame/app/src/test/java/com/game/wargame/Model/GameContext/GameContextTest.java
+++ AndroidApp/WarGame/app/src/test/java/com/game/wargame/Model/GameContext/GameContextTest.java
@@ -1,5 +1,7 @@
 package com.game.wargame.Model.GameContext;
 
+import com.game.wargame.Model.Entities.Players.Player;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,41 +22,48 @@ public class GameContextTest {
 
     GameNotification gn;
 
+    Player mKiller;
+    Player mKillee;
+
+
     @Before
     public void setUp() {
         mGameContext = new GameContext(mFragManager, new GameNotificationManager());
+        mGameContext.start(0);
+        mKiller = new Player("killer", "killer");
+        mKillee = new Player("killee", "killee");
     }
 
     @Test
     public void testThatNotificationIsAddedOnFrag() {
-        mGameContext.handleFrag("killer", "killee", 100);
+        mGameContext.handleFrag(mKiller, mKillee, 100);
         assertEquals(1, mGameContext.getNotificationsToDisplay().size());
     }
 
     @Test
     public void testThatThreeNotificationsAreReturned() {
-        mGameContext.handleFrag("killer", "killee", 100);
-        mGameContext.handleFrag("killer", "killee", 100);
-        mGameContext.handleFrag("killer", "killee", 100);
-        mGameContext.handleFrag("killer", "killee", 100);
+        mGameContext.handleFrag(mKiller, mKillee, 100);
+        mGameContext.handleFrag(mKiller, mKillee, 100);
+        mGameContext.handleFrag(mKiller, mKillee, 100);
+        mGameContext.handleFrag(mKiller, mKillee, 100);
         assertEquals(3, mGameContext.getNotificationsToDisplay().size());
     }
 
     @Test
     public void testThatNotificationsArePurged() {
-        mGameContext.handleFrag("killer", "killee", 0);
-        mGameContext.handleFrag("killer", "killee", 1);
+        mGameContext.handleFrag(mKiller, mKillee, 0);
+        mGameContext.handleFrag(mKiller, mKillee, 1);
         mGameContext.update(1, gn.TIME_TO_DISPLAY);
         assertEquals(1, mGameContext.getNotificationsToDisplay().size());
     }
 
     @Test
     public void testThatNotificationsAreSorted() {
-        mGameContext.handleFrag("killer1", "killee1", 2);
-        mGameContext.handleFrag("killer2", "killee2", 1);
-        mGameContext.handleFrag("killer3", "killee3", 0);
-        mGameContext.handleFrag("killer1", "killee1", 4);
-        mGameContext.handleFrag("killer1", "killee1", 5);
+        mGameContext.handleFrag(mKiller, mKillee, 2);
+        mGameContext.handleFrag(mKiller, mKillee, 1);
+        mGameContext.handleFrag(mKiller, mKillee, 0);
+        mGameContext.handleFrag(mKiller, mKillee, 4);
+        mGameContext.handleFrag(mKiller, mKillee, 5);
         assertEquals(0, (int) mGameContext.getNotificationsToDisplay().get(0).getStartTime());
         assertEquals(1, (int)mGameContext.getNotificationsToDisplay().get(1).getStartTime());
         assertEquals(2, (int)mGameContext.getNotificationsToDisplay().get(2).getStartTime());
