a58e2928d778f5bbc0290081be6bceeda7317b71
Clément Michaud <clement.michaud34@gmail.com>
2016-04-24 17:56:55 +0200

    Add the ability to trigger updates with command line input

--- game_controller/clock.js
+++ game_controller/clock.js
@@ -5,15 +5,30 @@ var clock_exchange = "clock_exchange";
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-function start() {
-    RabbitMQWrapper.initServerChannel("global_queue", function(){}, function() {
-	rl.on('line', function() {
-	  GameManager.updateClock()
-	}).on('close', function() {
-	  console.log('Have a great day!');
-	  process.exit(0);
-	});
+console.log(process.argv);
+
+function triggerUpdateWithCommandLine() {
+    rl.on('line', function() {
+        GameManager.updateClock()
+    }).on('close', function() {
+        console.log('Have a great day!');
+        process.exit(0);
     });
+}
+
+function triggerUpdateWithTimer() {
+    setInterval(GameManager.updateClock, 1000);
+}
+
+function start() {
+    var triggerMethod = triggerUpdateWithTimer;
+
+    if(process.argv.indexOf("--manual") >= 0) {
+        console.log("Manual trigger, you must type enter in the command line to trigger one tick");
+        triggerMethod = triggerUpdateWithCommandLine;
+    }
+
+    RabbitMQWrapper.initServerChannel("global_queue", function(){}, triggerMethod);
 }
 
 start();
--- run_server.sh
+++ run_server.sh
@@ -5,5 +5,5 @@ export AMQP_VIRTUAL_HOST=/$1
 echo "AMQP_VIRTUAL_HOST=$AMQP_VIRTUAL_HOST"
 
 nodejs game_controller/app.js &
-nodejs game_controller/clock.js 
+nodejs game_controller/clock.js --manual
 
